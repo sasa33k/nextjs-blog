f917b8dd72b4784bbde2648b6169e05317a1905b
sasa33k <ssamantha11@gmail.com>
2023-11-28 23:22:41 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    reactStrictMode: true,
-  
    // Add basePath
     basePath: '/nextjs-blog',
-    
-    output: 'export',
+    output: 'standalone',
   
   }
   
