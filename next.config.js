f18920771c80734f7369d7680be3d1d24ed2b4b7
sasa33k <ssamantha11@gmail.com>
2023-11-28 22:56:05 -0800

    next export config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    output: 'export',
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/github-pages',
+  
+  }
+  
+  module.exports = nextConfig
